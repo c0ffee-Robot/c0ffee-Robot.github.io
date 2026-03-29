0b3b60746fc92a242d56a8f0a4fa10fd661747ff
Takayuki <nitotkyk@gmail.com>
2026-03-29 11:47:18 +0900

    Todoリストの追加

---
+++ js/todo_list.js
@@ -0,0 +1,80 @@
+'use strict';
+{
+  const todoList = document.getElementById('todoList');
+
+  // --------------------------------------------------
+  // Todoの追加
+  // --------------------------------------------------
+  function addTodo() {
+    // 未入力は無効
+    const todoInput = document.getElementById('todoInput');
+    if (todoInput.value.trim() === '') return;
+
+    createTodoItem(todoInput.value.trim(), false);
+    todoInput.value = '';
+    saveTodos();
+  }
+
+  // --------------------------------------------------
+  // Todoリストの行を作成して追加
+  // --------------------------------------------------
+  function createTodoItem(text, checked) {
+    console.log(text);
+    console.log(checked);
+    // チェックボックス
+    const todoCheck = document.createElement('input');
+    todoCheck.type = 'checkbox';
+    todoCheck.classList.add('todo-check');
+    todoCheck.checked = checked;
+    // チェックボックスが変更されたら状態を保存する
+    todoCheck.addEventListener('change', saveTodos);
+    // 本文
+    const todoText = document.createElement('span');
+    todoText.classList.add('todo-text');
+    todoText.textContent = text;
+    // 削除ボタン
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'delete';
+    // 削除ボタンが押されたら対象のliタグを削除する
+    deleteButton.addEventListener('click', () => {
+      todoList.removeChild(li);
+      saveTodos();
+    });
+    // liタグ内に配置しリストに追加
+    const li = document.createElement('li');
+    li.appendChild(todoCheck);
+    li.appendChild(todoText);
+    li.appendChild(deleteButton);
+    todoList.appendChild(li);
+  }
+
+  // --------------------------------------------------
+  // Todoリストをローカルストレージに保存
+  // --------------------------------------------------
+  function saveTodos() {
+    const todos = [];
+    document.querySelectorAll('#todoList li').forEach(li => {
+      const checkbox = li.querySelector('.todo-check');
+      const text = li.querySelector('.todo-text').textContent;
+
+      todos.push({
+        text: text,
+        checked: checkbox.checked
+      });
+    });
+    localStorage.setItem('c0ffee-Robot_todo', JSON.stringify(todos));
+  }
+
+  // --------------------------------------------------
+  // ローカルストレージから画面表示リスト作成
+  // --------------------------------------------------
+  function loadTodos() {
+    const data = JSON.parse(localStorage.getItem('c0ffee-Robot_todo')) || [];
+    data.forEach(item => {
+      createTodoItem(item.text, item.checked);
+    });
+  }
+
+  document.getElementById('addTodo').addEventListener('click', addTodo);
+  document.addEventListener('DOMContentLoaded', loadTodos);
+}
